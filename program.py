083fb9ed6556d0f569e3e64a655aca207b3b8bb4
Johannes Pettersson <95682009+Johannes-Pettersson@users.noreply.github.com>
2022-01-31 21:34:01 +0100

    adding a detection for numbers in te in.txt file

--- program.py
+++ program.py
@@ -1,1 +1,11 @@
+with open('in.txt', encoding='utf-8') as i:
+    text = i.read()
+
+for element in text:
+    try:
+        num = int(element)
+    except:
+        print(element)
+    else:
+        print("nummer")
 
