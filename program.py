cb4fb0723cae1e78fce851103002ec00f221aaa0
Johannes Pettersson <95682009+Johannes-Pettersson@users.noreply.github.com>
2022-02-01 09:53:50 +0100

    adding a detecton for books

--- program.py
+++ program.py
@@ -3,33 +3,46 @@ with open('in.txt', encoding='utf-8') as i:
 
 num1 = "0"
 num2 = "0"
+bokprint = False
 
-for element in text:
+#bara kapitel kvar att lägga in
 
-    #printing chapter
-    try:
-        int(element)
-    except:
-        if num1 == "1":
-            print('<v n="' + num1 + '">')
-            print(element)
-            num1 = "0"
-        elif num1 != "0":
-            print('</v><v n="' + num1 + '">')
-            print(element)
-            num1 = "0"
+with open('out.txt', 'w') as o:
+
+    o.write('<?xml version="1.0" encoding="ISO-8859-1"?><bible>')
+
+    for element in text:
+
+        if element == "%":
+            bokprint = True
+            o.write('<b n="')
+            continue
+        elif element == "/":
+            bokprint = False
+            o.write('">')
+            continue
+        if bokprint:
+            o.write(element)
         else:
-            print(element)
-    else:
-        if num1 == "0":
-            num1 = element
-        else:
-            print('</v><v n="' + num1 + element + '">')
-            num1 = "0"
-        
+            #printing chapter
+            try:
+                int(element)
+            except:
+                if num1 == "1":
+                    o.write('<v n="' + num1 + '">')
+                    o.write(element)
+                    num1 = "0"
+                elif num1 != "0":
+                    o.write('</v><v n="' + num1 + '">')
+                    o.write(element)
+                    num1 = "0"
+                else:
+                    o.write(element)
+            else:
+                if num1 == "0":
+                    num1 = element
+                else:
+                    o.write('</v><v n="' + num1 + element + '">')
+                    num1 = "0"
 
-
-
-
-        
-
+    o.write('</b></v></bible>')
